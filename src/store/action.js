a4501d0ce1795707abb4a45e4eb5ddc459483ff0
zhuyue <kid1412-yuki@sohu.com>
2017-12-06 10:32:13 +0800

    no message

--- src/store/action.js
+++ src/store/action.js
@@ -1,11 +1,11 @@
 /**
  * Created by zhuyue on 17/12/5.
  */
-const ADD_TODO = 'ADD_TODO'
-
-{
-  type: ADD_TODO,
-    text: 'Build my first Redux app'
-}
-
-import { ADD_TODO, REMOVE_TODO } from '../actionTypes'
+// const ADD_TODO = 'ADD_TODO'
+//
+// {
+//   type: ADD_TODO,
+//     text: 'Build my first Redux app'
+// }
+//
+// import { ADD_TODO, REMOVE_TODO } from '../actionTypes'
